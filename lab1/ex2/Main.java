13b6ef813ae6d4482adb0bc1918f4e6759c41fa9
Szabo Agnes <agnes.szabo@student.upt.ro>
2024-02-28 08:13:45 +0200

    tema

--- lab1/ex2/Main.java
+++ lab1/ex2/Main.java
@@ -1,46 +1,40 @@
 package ex2;
 
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        System.out.print("Introduceți un număr natural: ");
-        int numar = scanner.nextInt();
+        String fis_in = "ex2/in.txt";
+        String fis_out = "ex2/out.txt";
+        BufferedReader flux_in;
+        PrintStream flux_out;
+        flux_in = new BufferedReader(new FileReader(fis_in));
+        flux_out = new PrintStream(new FileOutputStream(fis_out));
 
-        System.out.println("Divizorii numărului " + numar + " sunt:");
-        afiseazaDivizorii(numar);
+        int suma = 0, minim = Integer.MAX_VALUE, maxim = Integer.MIN_VALUE, nr = 0;
+        float media;
 
-        if (estePrim(numar)) {
-            System.out.println(numar + " este un număr prim.");
-        } else {
-            System.out.println(numar + " nu este un număr prim.");
+        String line;
+        while ((line = flux_in.readLine()) != null) {
+            int numar = Integer.parseInt(line);
+
+            suma = suma + numar;
+            if (minim > numar)
+                minim = numar;
+            if (maxim < numar)
+                maxim = numar;
+
+            nr++;
         }
 
-        scanner.close();
-    }
+        media = (float) suma / nr;
+        flux_out.println("SUMA: " + suma);
+        flux_out.println("MAXIMUL: " + maxim);
+        flux_out.println("MINIMUL: " + minim);
+        flux_out.println("MEDIA ARITMETICA: " + media);
 
-    // Metoda pentru a afișa divizorii unui număr
-    public static void afiseazaDivizorii(int numar) {
-        for (int i = 1; i <= numar; i++) {
-            if (numar % i == 0) {
-                System.out.print(i + " ");
-            }
-        }
-        System.out.println(); // Trecem la o nouă linie după afișarea divizorilor
-    }
-
-    // Metoda pentru a verifica dacă un număr este prim
-    public static boolean estePrim(int numar) {
-        if (numar <= 1) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(numar); i++) {
-            if (numar % i == 0) {
-                return false;
-            }
-        }
-        return true;
+        flux_in.close();
+        flux_out.close();
     }
 }
